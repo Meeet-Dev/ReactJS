c7f049db8d3e2a1c2d9f56a28ac92185b665687e
Meeet-Dev <jadavmeet2203@gmail.com>
2025-04-12 09:05:39 +0530

    Update App.jsx

--- ReactProps/src/App.jsx
+++ ReactProps/src/App.jsx
@@ -1,1 +1,21 @@
+import './App.css'
+import ProfileCard  from './ProfileCard';
+import ProfileCardList from './ProfileCardList';
+
+export default function App() {
+  return (
+    <main>
+      {ProfileCardList.map((profile) => {
+        return (
+          <ProfileCard
+            name={profile.name}
+            age={profile.age}
+            country={profile.country}
+            title={profile.title}
+            />
+        )
+      })}
+    </main>
+  )
+}
 
